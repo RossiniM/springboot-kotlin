42879e760def0b77af74f95fce376bb16954aec2
RossiniM <rossini1408@gmail.com>
2020-12-27 16:50:31 -0300

    - include setting to enable import sql script - impl script to mock data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO tbl_promotion(amount_days, description, is_all_inclusive, local, price)
+VALUES (10,'montanhas canadenses',1, 'Canadá', 5000.0);
+
+insert into tbl_promotion(amount_days, description, is_all_inclusive, local, price)
+values (5, 'Serra do rio do rastro', 0, 'Brazil', 500.0);
+
+insert into tbl_promotion(amount_days, description, is_all_inclusive, local, price)
+values (30, 'Amazonas', 0, 'Brasil', 2000.0);
+
+insert into tbl_promotion(amount_days, description, is_all_inclusive, local, price)
+values (15, 'São Paulo', 1, 'Brasil', 2000.0);
+
+insert into tbl_promotion(amount_days, description, is_all_inclusive, local, price)
+values (25, 'Nordeste', 1, 'Brasil', 500.0);
